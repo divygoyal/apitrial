89c43ec0546418e85323af4a9a6193a484d0af0d
divy goyal <goyaldivy1010@gmail.com>
2023-04-18 23:43:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose'); 
+const app = express();
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+dotenv.config({path:'./.env'})
+require('./Db/Conn')
+
+
+const PORT = process.env.PORT;
+
+const User = require('./UserSchema');
+const { json } = require('express');
+
+
+const middleware =(req,res,next)=>{
+    console.log("middleware mf");
+    next();
+}
+app.use(cookieParser());
+app.use(express.json());
+
+console.log("hello");
+app.use((require('./router/Auth')));
+// app.use((require('./router/Auth')))
+
+// app.get('/signin' , (req,res)=>{
+//     res.send('hello');
+// })
+
+app.get('/register' , middleware , (req,res)=>{
+    res.send('hello');
+})
+app.get('/contact' , (req,res)=>{
+    res.send('hello');
+})
+
+app.listen(PORT,()=>{
+    console.log(`running on ${PORT}`)
+})
