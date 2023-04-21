deaf3d545ebbc8b61d3e1d82c7a079b9e9b60e87
Divy Goyal <86590133+divygoyal@users.noreply.github.com>
2023-04-21 11:43:49 +0530

    Update Auth.js

--- router/Auth.js
+++ router/Auth.js
@@ -100,7 +100,8 @@ router.post('/signin' , async (req,res)=>{
             console.log(token);
             res.cookie("jwtoken", token, {
                 expires: new Date(Date.now() +25892000000),
-                httpOnly: true
+                httpOnly: true,
+                secure: true
             });
             
             if(matchornot){
